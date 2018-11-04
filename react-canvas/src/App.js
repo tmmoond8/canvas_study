7d04f69313794a1ea6591b6b417c7b1a6b39717c
Taemin Moon <tmmoond8@gmail.com>
2018-11-04 13:23:56 +0900

    react에 canvas를 사용하는 기본적인 방법

---
+++ react-canvas/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import CanvasComponent from './CanvasComponent';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <CanvasComponent/>
+      </div>
+    );
+  }
+}
+
+export default App;
